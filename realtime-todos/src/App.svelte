5279c7008066d8219ac3a396bfe4f3797056e9ab
Chris Catwell <christophercantwell@college.harvard.edu>
2023-01-30 14:48:36 -0500

    Finish Mozilla tutorial

--- realtime-todos/src/App.svelte
+++ realtime-todos/src/App.svelte
@@ -1,9 +1,15 @@
 <script>
-	export let name;
 	import Todos from "./components/Todos.svelte";
+
+	let todos = [
+    	{ id: 1, name: "Create a Svelte starter app", completed: true },
+    	{ id: 2, name: "Create your first component", completed: true },
+    	{ id: 3, name: "Complete the rest of the tutorial", completed: false }
+	];
+
 </script>
 
-<Todos />
+<Todos todos={todos}/>
 
 <main>
 	<p>Built with guidance from 

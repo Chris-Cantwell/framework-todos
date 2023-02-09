715acb601e3c476e5181e95a7ecb221d384426d1
Chris Catwell <christophercantwell@college.harvard.edu>
2023-02-09 15:28:19 -0500

    Differentiate Completed and Non-Completed Tasks

--- Flask/app.py
+++ Flask/app.py
@@ -3,11 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-todos = ["Flask Tutorial", "MongoDB Tutorial", "Readings for Other Classes"]
+class task ():
+    def __init__ (self, name, status):
+        self.name = str(name)
+        self.completed = bool(status)
+
+todos = [task(name="Flask Tutorial", status=False), 
+         task(name="MongoDB Tutorial", status=False), 
+         task(name="191 Readings", status=False),
+         task(name="Completed Task", status=True)]
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html", todos=todos)
+    return render_template("todos.html", todos=todos)
 
 if __name__ == "__main__":
     app.run(debug=True)

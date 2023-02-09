626982dc4f8ffc921f171bb6858e9ec7d0e8a9c9
Chris Catwell <christophercantwell@college.harvard.edu>
2023-02-09 13:52:51 -0500

    Opening new repo

---
+++ Svelte/README.md
@@ -0,0 +1,5 @@
+## ToDo App
+
+Introductory example to working in Svelte.js, borrowing from [Fireship](https://fireship.io/lessons/svelte-v3-overview-firebase/) and [Mozilla](https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_Todo_list_beginning) tutorials as well as the [Svelte docs](https://svelte.dev/blog/svelte-for-new-developers). 
+
+Done as part of the first few assignments in COMPSCI 178: Engineering Usable Interactive Systems. 
---
+++ Svelte/realtime-todos/src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import Todos from "./components/Todos.svelte";
+
+	// #Props
+	let todos = [
+    	{ id: 1, name: "Create a Svelte starter app", completed: true },
+    	{ id: 2, name: "Create your first component", completed: true },
+    	{ id: 3, name: "Complete the rest of the tutorial", completed: false }
+	];
+
+</script>
+
+<!-- #Component -->
+<Todos todos={todos}/>
+
+<main>
+	<p>Built with guidance from 
+		<a href=https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Svelte_Todo_list_beginning>Mozilla's tutorial</a></p>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		font-style: italic;
+		color:gray;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>

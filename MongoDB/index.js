e848c107911d9172b49272383642b84cc6aa209e
Chris Catwell <christophercantwell@college.harvard.edu>
2023-02-15 15:25:20 -0500

    Updated Node, tutorial progress

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -2,17 +2,33 @@
 const express = require("express");
 const app = express();
 
-// Access CSS
-app.use("/static", express.static("public"));
+const dotenv = require("dotenv")
+dotenv.config();
+
+const mongoose = require("mongoose");
+
+// Configuration
+app.use("/static", express.static("public")); // Access CSS
+app.use(express.urlencoded({extended: true}));
+
+// Connect to DB, Launch Server    
+// mongoose.set("useFindAndModify", false); -- indicated by tutorial, throws an error
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+});
 
 /* #Template */
-// Access Embedded JS Files
-app.set("view engine", "ejs");
+// Configure view engine
+app.set("view engine", "ejs"); // Access Embedded JS Files
 
-// GET Request
-app.get('/',(req, res) => {
+/* Routing */
+app.get('/',(req, res) => { 
     res.render('todo.ejs');
-    });
+    }); // GET Request, renders page
 
-// Launch Server    
-app.listen(3000, () => console.log("Server Up and running"));
+app.post('/', (req, res) => {
+    console.log(req.body);
+}); // POST Method
+    
+
